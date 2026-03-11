99b3c62081c1f59ac0940a12b4230ca2e42a2b37
Angel <madeuxa2@southernct.edu>
2026-03-11 13:11:37 -0400

    Initialize Flask app with home and recipe routes

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/recipe")
+def recipe():
+    return render_template("recipe.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
